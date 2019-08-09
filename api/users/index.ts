4b7f25ee7fe34fa79a51fb4cd8ddc7b5375f47b5
Robin Andeer <robin.andeer@gmail.com>
2019-08-09 10:56:27 +0200

    Add cors headers

--- api/users/index.ts
+++ api/users/index.ts
@@ -5,5 +5,10 @@ import { User } from "../../lib/types";
 const USERS: User[] = require("../../data/users.json");
 
 export default (_req: NowRequest, res: NowResponse) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+
   res.json({ users: USERS });
 };
