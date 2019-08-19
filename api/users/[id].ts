78c4809487c5fd1ef69d4c6f18a1ed8bcdd35855
Robin Andeer <robin.andeer@gmail.com>
2019-08-19 14:28:56 +0200

    Update test data

--- api/users/[id].ts
+++ api/users/[id].ts
@@ -5,6 +5,11 @@ import { User } from "../../lib/types";
 const USERS: User[] = require("../../data/users.json");
 
 export default (req: NowRequest, res: NowResponse) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Request-Method", "*");
+  res.setHeader("Access-Control-Allow-Methods", "OPTIONS, GET");
+  res.setHeader("Access-Control-Allow-Headers", "*");
+
   const userId = req.query.id;
   const user = USERS.find(user => user.id === userId);
 
--- tests/client.js
+++ tests/client.js
@@ -1,3 +1,5 @@
 import axios from "axios";
 
+jest.setTimeout(60000);
+
 export default axios.create({ baseURL: process.env.BASE_URL });
